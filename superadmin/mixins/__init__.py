0a185588419558d7cf7e383e8333e93df2dd2a37
Denis Siavichay <dbsiavichay@gmail.com>
2021-05-21 16:34:39 -0500

    :package: Package files

--- superadmin/mixins/__init__.py
+++
@@ -1,6 +0,0 @@
-from .formsets import FormsetMixin
-from .breadcrumbs import BreadcrumbMixin
-from .urls import UrlMixin
-from .templates import TemplateMixin
-from .permissions import PermissionRequiredMixin
-from .filters import FilterMixin
--- superadmin/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from .list import ListView
-from .create import CreateView
-from .update import UpdateView, MassUpdateView
-from .detail import DetailView
-from .delete import DeleteView, MassDeleteView
-from .duplicate import DuplicateView
-from .filter import FilterView
-from .base import ModuleView
