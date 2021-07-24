0f91c0317d77b8b2e4f1988098177c6c98a402f4
Denis Siavichay <dbsiavichay@gmail.com>
2021-07-24 14:11:40 -0500

    :package: Package files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-superadmin",  # Replace with your own username
-    version="1.2.2",
+    version="1.2.3",
     author="Denis Siavichay",
     author_email="dbsiavichay@gmail.com",
     description="A Django app for build admin sites like django admin.",
